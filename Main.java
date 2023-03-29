4199eaf8abefa59048eff2f51722b7286b8d7e9c
svh0310 <125602397+svh0310@users.noreply.github.com>
2023-03-29 11:31:25 -0500

    Creacion de usuario y contrasena en Java
    Pasamos el requisito de crear un usuario con contrasena a codigo.

--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
-public class Main 
-{
-  public static void Main(String[] args)
-  {
-  
-  }
+import java.util.Scanner;
 
+public class Main {
 
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ingrese el nombre de usuario: ");
+        String usuario = sc.nextLine();
+        
+        System.out.print("Ingrese la contraseña: ");
+        String password = sc.nextLine();
+        
+        System.out.println("Usuario creado exitosamente:");
+        System.out.println("Usuario: " + usuario);
+        System.out.println("Contraseña: " + password);
+
+    }
 
 }
